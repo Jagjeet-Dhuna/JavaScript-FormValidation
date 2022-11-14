afa9c23c985cc90400ca8ce0515e7c2f2e61c0a9
Jagjeet Dhuna <48265165+Jagur360@users.noreply.github.com>
2022-11-14 15:44:41 +0000

    email now validates

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const password = document.getElementById('password')
 const confirmpassword = document.getElementById('confirmpassword')
 const errorElement = document.getElementById('error1')
 
-var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var vaildPasswordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
 
 var isValid1 = false
 var isValid2 = false
 var isValid3 = false 
+var isValid4 = false
 
 
 form.addEventListener('submit', (e) => {
@@ -20,11 +22,10 @@ form.addEventListener('submit', (e) => {
 
     //email errors
 
-    if (email.value.match(validRegex)) {
-        set.isValid1 = true
-    } else {
-        messages.push('Invalid email address')
-    }
+
+    if (email.value.match(validEmailRegex)) {
+        isValid1 = true
+    } else (messages.push('Invalid email'))
 
     //username errors
     if (username.value === '' || username.value == null, username.value.length <= 2) {
@@ -35,14 +36,16 @@ form.addEventListener('submit', (e) => {
 
     //password errors
 
-    if (password.value != confirmpassword.value){
+    if (password.value.match(vaildPasswordRegex) || password.value.length >= 7) {
+        isValid3 = true
+    } else messages.push('Invalid password. password must be atleasr 8 characters and contain atleast 1 Uppercase, lowercase and special character.')
+
+    if (password.value != confirmpassword.value) {
         messages.push('passwords do not match')
-        } else if (password.value.length <= 2) {
-            messages.push('Invalid Password')
-        } else (isValid3=true)
-        
-    if (isValid2 && isValid3 == true){
-        location.replace("./home.html")
+    } else (isValid4 = true)
+                
+    if (isValid1 && isValid2 && isValid3 && isValid4 == true){
+        location.replace("./success.html")
     }
     //location.replace("./home.html")
     //messages.push('SUCCESS!')
